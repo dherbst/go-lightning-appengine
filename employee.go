db655ce410b278826efa40d09214ff662ada7646
Darrel Herbst <dherbst@gmail.com>
2014-12-09 09:18:25 -0500

    converted from google presentation to go-present

---
+++ employee.go
@@ -0,0 +1,13 @@
+package employee
+
+import (
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/index.html", http.StatusFound)
+}
